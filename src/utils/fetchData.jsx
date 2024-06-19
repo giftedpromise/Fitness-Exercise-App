e7c0bb8caafb0be583648b216efb8cc2ff8c80f1
giftedpromise <giftedpromise1992@gmail.com>
2024-06-18 23:50:30 -1200

    integrated API

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -6,7 +6,9 @@ export const exerciseOptions = {
   },
 };
 
-
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
 
   return data;
 };
