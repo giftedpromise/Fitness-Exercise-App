cb72660edbef347d9f8a26bbc8acc1bd14cc4146
giftedpromise <giftedpromise1992@gmail.com>
2024-06-19 02:56:31 -1200

    body parts

---
+++ src/components/HorizontalScrollbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Box } from "@mui/material";
+import BodyParts from "./BodyParts";
+
+const HorizontalScrollbar = ({ data, bodypart, setBodyPart }) => {
+  return (
+    <div>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+        >
+          <BodyParts
+            item={item}
+            bodypart={bodypart}
+            setBodyPart={setBodyPart}
+          />
+        </Box>
+      ))}
+    </div>
+  );
+};
+
+export default HorizontalScrollbar;
